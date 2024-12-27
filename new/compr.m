2fd3cb2c5a14fe78e80e0a891ac376811d731c20
Ben Stein <whereyouben@gmail.com>
2024-12-27 13:20:21 +0100

    fixed compr function

--- new/compr.m
+++ new/compr.m
@@ -4,7 +4,7 @@ function output = compr(x,comp,a)
     % a - filter parameter <l
     h = filter((1-a)^2, [1.0000 -2*a a^2], abs(x));
     h = h / max(h);
-    h = h .^ comp;
+    h = h .^ comp; 
     y = x .* h;
-    output = y .* max(abs(x)) ./ max(abs(y));
+    output = y .* max(abs(x)) / max(abs(y));
 end
